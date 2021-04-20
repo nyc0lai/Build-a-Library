c8f09e6b0560620389a21500715d324675422bbf
nyc0lai <nyc0lai@gmail.com>
2021-04-20 03:26:22 +0300

    third comit

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 class Media {
-    constructor(title, isCheckedOut, ratings){
+    constructor(title){
         this._title = title;
-        this._isCheckedOut = isCheckedOut;
-        this._ratings = ratings;
-    }
-
-    get author() {
-        return this._author;
+        this._isCheckedOut = false;
+        this._ratings = [];
     }
 
     get title() {
@@ -16,26 +12,47 @@ class Media {
     get isCheckedOut() {
         return this._isCheckedOut;
     }
-
-    getAverageRating() {
-
+    
+    get ratings() {
+        return this._ratings;
     }
+    
+    getAverageRating() {
+        if(this.ratings.length === 0)
+        return `has not been evaluated`; else
+        return this.ratings.reduce((num1, num2) => num1+num2)/this.ratings.length.toFixed(2);
+        }
 
     toggleCheckOutStatus() {
+        if(this.isCheckedOut === false)
+        this.isCheckedOut = true;
+            else
+        this.isCheckedOut = false;
+        }
+    
 
-    }
-
-    addRating() {
-
+    addRating(val) {
+        if(typeof val !== 'number')
+        return `Enter a number`;
+        else
+        this._ratings.push(val);
+        
     }
 
 };
 
 class Book extends Media {
-    constructor(author, title, pages, isCheckedOut, ratings) {
+    constructor(author, pages, title, isCheckedOut, ratings) {
         super(title, isCheckedOut, ratings);
         this._author = author;
         this._pages = pages;
+    }
+
+    get author() {
+        return this._author;
+    }
+    get pages() {
+        return this._pages;
     }
 
 };
@@ -44,17 +61,42 @@ class Movie extends Media{
     constructor(director, title, runTime, isCheckedOut, ratings){
         super(title, isCheckedOut, ratings);
         this._director = director;
-        this,_runTime = runTime;
+        this._runTime = runTime;
+    }
+
+    get director() {
+        return this._director;
+    }
+    get runTime() {
+        return this._runTime;
     }
 
 };
 
 class CD extends Media{
-    constructor(artist, title, isCheckedOut, ratings, songs){
+    constructor(artist, songs, title, isCheckedOut, ratings){
         super(title, isCheckedOut, ratings);
         this._artist = artist;
         this._songs = songs;
     }
 
+    get artist() {
+        return this._artist;
+    }
+    get songs() {
+        return this._songs;
+    }
+
 };
 
+// enter data in this format: ('director', 'title', run hours in minute ex: 120);
+const film1 = new Movie ('Director', 'Title', 90);
+film1.addRating(7);
+film1.addRating(9);
+film1.addRating(3);
+film1.getAverageRating();
+console.log(film1);
+
+// enter data in this format: ('artist', ['song1', 'song2', ... 'song n'], title);
+const cd1 = new CD ('Dan Balad',['Dragostea din tei', 'foaie verde', 'chiperi iute'], 'Muzica de estrada anii 90');
+console.log(cd1);
